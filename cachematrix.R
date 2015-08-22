5d755ae21aa453b50deed47bdcea1cdfb9ba9683
chiaminhsu <chiaminhsu@gmail.com>
2015-08-23 01:45:47 +0800

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL 
+  }
+  get <- function() x
+  setInverse <- function(inv) i <- inv
+  getInverse <- function() i
+  list(set = set, get = get, 
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- inverse.gaussian(data)
+  x$setInverse(m)
+  m
 }
